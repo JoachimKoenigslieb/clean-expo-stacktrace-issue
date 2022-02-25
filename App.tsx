617942e48f4585e1c941ccfb62910360d9c9f9c1
Joachim Kønigslieb <joachim.koenigslieb@gmail.com>
2022-02-25 17:54:04 +0100

    intentional bug commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+	flex: 1,
+	backgroundColor: '#fff',
+	alignItems: 'center',
+	justifyContent: 'center',
+  },
+});
+
+interface Comp {
+	body: string,
+}
+
+const Component = ({ body, }: Comp) => {
+	return (
+		<Text>
+			din mor g er du her {body}
+		</Text>
+	)
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Very cool app laver goe ting</Text>
+	  {!Component()}
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
